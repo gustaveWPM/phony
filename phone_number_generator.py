9526fdb86f67bba3a767d6f682fef8990433dc73
gustaveWPM <jgeorge@perso.be>
2023-02-18 00:10:18 +0100

    add: database persistence

--- phone_number_generator.py
+++ phone_number_generator.py
@@ -1,12 +1,30 @@
-DEBUG_MODE = True
+import pymongo
 
-PREFIX = "+336" # * ... Le prefixe du numéro de téléphone
-NDIGITS = 8 # * ... Le nombre de numéros dans ton suffixe
-SAME_DIGIT_THRESHOLD = 4 # * ... Le nombre maximum du même numéro dans le bloc généré
+NDIGITS = 8 # * ... Number of digits in the pseudo randomly generated block
+SAME_DIGIT_THRESHOLD = 4 # * ... Maximum same digit amount in the generated block
+MONGO_DB_CONNECTION_ROUTE = "mongodb://localhost:27017/"
+
+PREFIX_DATA = {
+    "COUNTRY_CODE": "33",
+    "OPERATOR_CODE": "6"
+}
 
 # * ... Don't edit this unless you know what you are doing
+DEBUG_MODE = False
 DIGITS = "0123456789"
 MAGNITUDE = 10 ** (NDIGITS - 1)
+
+MONGO_CLIENT = pymongo.MongoClient(MONGO_DB_CONNECTION_ROUTE)
+DB = MONGO_CLIENT["phone_book"]
+DB_COL = DB["hungary"]
+
+def retrieve_last_saved_phone_number_suffix() -> str:
+    try:
+        last_saved_phone_number_entry = DB_COL.find_one(sort=[( '_id', pymongo.DESCENDING )])
+        last_saved_phone_number = last_saved_phone_number_entry["generated_suffix"]
+        return last_saved_phone_number
+    except:
+        return '0'
 
 def reject_phone_number_suffix(phone_number_suffix) -> bool:
     head_max_zeros = 2
@@ -19,10 +37,20 @@ def reject_phone_number_suffix(phone_number_suffix) -> bool:
             return True
     return False
 
-def save_phone_number(phone_number):
+def save_phone_number(phone_number: str, prefix_data: dict, phone_number_suffix: str):
+    country_code = prefix_data["COUNTRY_CODE"]
+    operator_code = prefix_data["OPERATOR_CODE"]
+
+    database_entry = {
+        "phone_number": phone_number,
+        "country_code": country_code,
+        "operator_code": operator_code,
+        "generated_suffix": phone_number_suffix
+    }
+
+    DB_COL.insert_one(database_entry)
     if (DEBUG_MODE):
         print(f"[DEBUG] Generated phone number: {phone_number}")
-    # * ... {ToDo} Sauvegarde phone_number
 
 def append_heading_zeros(number, ndigits):
     number_as_string = str(number)
@@ -33,13 +61,15 @@ def append_heading_zeros(number, ndigits):
     phone_suffix = '0' * number_of_zeros_to_append + number_as_string
     return phone_suffix
 
-def do_generate(ndigits: int, prefix: str, first_iteration: int = 0):
+def do_generate(ndigits: int, prefix_data: dict, first_iteration: int = 0):
     max_iter = int('9' * (SAME_DIGIT_THRESHOLD + 1) + '0' * abs(NDIGITS - SAME_DIGIT_THRESHOLD)) // 10 + 1 # * ... lol
+    prefix = prefix_data["COUNTRY_CODE"] + prefix_data["OPERATOR_CODE"]
+
     for i in range(first_iteration, max_iter):
         current_phone_number_suffix = append_heading_zeros(i, ndigits)
         if (not reject_phone_number_suffix(current_phone_number_suffix)):
             currrent_phone_number = prefix + current_phone_number_suffix
-            save_phone_number(currrent_phone_number)
+            save_phone_number(currrent_phone_number, prefix_data, current_phone_number_suffix)
 
 def config_error_handling():
     if (NDIGITS < SAME_DIGIT_THRESHOLD):
@@ -51,9 +81,10 @@ def config_error_handling():
 
 def run_phone_numbers_generator():
     config_error_handling()
-    prefix = PREFIX
+    prefix_data = PREFIX_DATA
     ndigits = NDIGITS
-    do_generate(ndigits, prefix)
+    first_iteration = int(retrieve_last_saved_phone_number_suffix()) + 1
+    do_generate(ndigits, prefix_data, first_iteration)
     print("Mission complete!")
 
 if __name__ == "__main__":
