b32bf3b8eecb239b28fc18f5dce809c461ac0333
gustaveWPM <jgeorge@perso.be>
2023-02-18 15:45:06 +0100

    fix: code inconsistencies

--- src/phone_number_generator.py
+++ src/phone_number_generator.py
@@ -10,7 +10,7 @@ FORCED_OPERATOR_CODES = []   # * ... Default value is `[]`
 VERY_FIRST_ITERATION = "0"   # * ... Default value is `"0"`
 
 
-def reject_phone_number_suffix(phone_number_suffix: str) -> bool:
+def reject_phone_number_suffix(operator_code: str, phone_number_suffix: str) -> bool:
     head_max_zeros = GEN_CONF["HEAD_MAX_ZEROS"]
     same_digit_threshold = GEN_CONF["SAME_DIGIT_THRESHOLD"]
     same_consecutive_digit_threshold = GEN_CONF["CONSECUTIVE_SAME_DIGIT_THRESHOLD"]
@@ -51,24 +51,25 @@ def do_generate(ndigits: int, prefix_data: dict, first_iteration: int):
     country_code = prefix_data["COUNTRY_CODE"]
     head_max_zeros = GEN_CONF["HEAD_MAX_ZEROS"]
 
-    for current_operator_code in prefix_data["OPERATOR_CODES"]:
-        prefix = country_code + current_operator_code
-        computed_ndigits = ndigits - len(current_operator_code)
+    for cur_operator_code in prefix_data["OPERATOR_CODES"]:
+        prefix = country_code + cur_operator_code
+        computed_ndigits = ndigits - len(cur_operator_code)
         magnitude = 10 ** (computed_ndigits - 1)
 
         if (head_max_zeros == 0 and first_iteration < magnitude):
             first_iteration = magnitude
 
         for current_iteration in range(first_iteration, max_iteration):
-            current_phone_number_suffix = append_heading_zeros(
+            cur_phone_number_suffix = append_heading_zeros(
                 current_iteration, computed_ndigits, magnitude)
-            if (not reject_phone_number_suffix(current_phone_number_suffix)):
-                currrent_phone_number = prefix + current_phone_number_suffix
-                save_phone_number(currrent_phone_number, country_code,
-                                  current_operator_code, current_phone_number_suffix)
+
+            if (not reject_phone_number_suffix(cur_operator_code, cur_phone_number_suffix)):
+                cur_phone_number = prefix + cur_phone_number_suffix
+                save_phone_number(cur_phone_number, country_code,
+                                  cur_operator_code, cur_phone_number_suffix)
                 if (DEBUG_MODE):
                     print(
-                        f"[DEBUG] Generated phone number: {currrent_phone_number}")
+                        f"[DEBUG] Generated phone number: {cur_phone_number}")
 
 
 def compute_first_iteration_value(metadatas):
@@ -131,21 +132,22 @@ def run_phone_numbers_generator():
     ndigits = GEN_CONF["NDIGITS"]
     last_saved_phone_metadatas = retrieve_last_saved_phone_metadatas()
 
-    if (skip_generation(last_saved_phone_metadatas)):
+    a_metadatas = last_saved_phone_metadatas
+    a_op_codes = prefix_data["OPERATOR_CODES"]
+
+    if (skip_generation(a_metadatas)):
         print("You already have a finite phonebook.")
         return
 
     if (FORCED_FIRST_ITERATION != -1 and UNSAFE):
         first_iteration = FORCED_FIRST_ITERATION
     else:
-        first_iteration = compute_first_iteration_value(
-            last_saved_phone_metadatas)
+        first_iteration = compute_first_iteration_value(a_metadatas)
 
     if (FORCED_OPERATOR_CODES and UNSAFE):
         prefix_data["OPERATOR_CODES"] = FORCED_OPERATOR_CODES
     else:
-        prefix_data["OPERATOR_CODES"] = compute_operator_codes_slice(
-            last_saved_phone_metadatas, prefix_data["OPERATOR_CODES"])
+        prefix_data["OPERATOR_CODES"] = compute_operator_codes_slice(a_metadatas, a_op_codes)
 
     do_generate(ndigits, prefix_data, first_iteration)
     append_finite_collection_indicator()
