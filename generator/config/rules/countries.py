7e223bec8ef346fffe2e703f6b71d0e7f6bb1efe
gustaveWPM <jgeorge@perso.be>
2023-02-19 00:48:41 +0100

    (minor changes)

--- generator/config/rules/countries.py
+++ generator/config/rules/countries.py
@@ -2,9 +2,11 @@
 
 from functools import reduce
 
+
 def _uniq(l: list) -> list:
     l_uniq = reduce(lambda re, x: re+[x] if x not in re else re, l, [])
     return l_uniq
+
 
 COUNTRIES: dict = {
     "FRANCE": {
