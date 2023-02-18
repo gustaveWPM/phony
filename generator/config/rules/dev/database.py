03cdfdf8e4b7406595f525fccb1dd7a6f25d87c4
gustaveWPM <jgeorge@perso.be>
2023-02-18 21:50:25 +0100

    (idem)

--- generator/config/rules/dev/database.py
+++ generator/config/rules/dev/database.py
@@ -4,6 +4,7 @@ DB: dict = {
     "DISABLE_PERSISTENCE": True,
     "MONGO_DB_CONNECTION_URI": "mongodb://localhost:27017/",
     "MONGO_DB_NAME": "phonebook",
+
     "MONGO_DB_TABLE": "{BUILDER}"
 }
 
