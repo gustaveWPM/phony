11e89e61c066d83c944f55b14c1c5387e19074bf
gustaveWPM <jgeorge@perso.be>
2023-02-18 01:09:29 +0100

    review: some encapsulation

--- db.py
+++ db.py
@@ -5,37 +5,37 @@ MONGO_CLIENT = pymongo.MongoClient(DB_CONF["MONGO_DB_CONNECTION_ROUTE"])
 DB = MONGO_CLIENT["phone_book"]
 DB_TABLE = DB["france"]
 
-def retrieve_last_saved_phone_number_entry():
+def _retrieve_last_saved_phone_number_entry():
     try:
         last_saved_phone_number_entry = DB_TABLE.find_one(sort=[( "_id", pymongo.DESCENDING )])
         return last_saved_phone_number_entry
     except:
         return None
 
-def retrieve_last_saved_phone_operator_code(entry) -> str:
+def _retrieve_last_saved_phone_operator_code(entry) -> str:
     if (entry is None):
         return "-1"
     data = entry["operator_code"]
     return data
 
-def retrieve_last_saved_phone_country_code(entry) -> str:
+def _retrieve_last_saved_phone_country_code(entry) -> str:
     if (entry is None):
         return "-1"
     data = entry["country_code"]
     return data
 
-def retrieve_last_saved_phone_number_suffix(entry) -> str:
+def _retrieve_last_saved_phone_number_suffix(entry) -> str:
     if (entry is None):
         return '0'
     data = entry["generated_suffix"]
     return data
 
 def retrieve_last_saved_phone_metadatas():
-    entry = retrieve_last_saved_phone_number_entry()
+    entry = _retrieve_last_saved_phone_number_entry()
     if (entry is None):
         return None
     metadatas = {}
-    metadatas["phone_number_suffix"] = retrieve_last_saved_phone_number_suffix(entry)
-    metadatas["phone_number_country_code"] = retrieve_last_saved_phone_country_code(entry)
-    metadatas["phone_number_operator_code"] = retrieve_last_saved_phone_operator_code(entry)
+    metadatas["phone_number_suffix"] = _retrieve_last_saved_phone_number_suffix(entry)
+    metadatas["phone_number_country_code"] = _retrieve_last_saved_phone_country_code(entry)
+    metadatas["phone_number_operator_code"] = _retrieve_last_saved_phone_operator_code(entry)
     return metadatas
