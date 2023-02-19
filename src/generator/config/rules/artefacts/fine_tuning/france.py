e5d73a08b2612c9bcfc33a20a7a1da1c7942e3eb
gustaveWPM <jgeorge@perso.be>
2023-02-19 18:28:49 +0100

    review: some documentation edit

--- src/generator/config/rules/artefacts/fine_tuning/france.py
+++ src/generator/config/rules/artefacts/fine_tuning/france.py
@@ -3,11 +3,11 @@
 FINE_TUNING: dict = {
     # * ... Number of digits in the complete phone suffix, operator code included
     "NDIGITS": 9,
-    # * ... Maximum same digit amount in the generated block
+    # * ... Maximum same digit amount in the generated pseudo-randomly block
     "SAME_DIGIT_THRESHOLD": 5,
-    # * ... Maximum consecutive same digit amount anywhere in the generated block
+    # * ... Maximum consecutive same digit amount anywhere in the pseudo-randomly generated block
     "CONSECUTIVE_SAME_DIGIT_THRESHOLD": 4,
-    # * ... Maximum consecutive 0 amount in the beginning of the generated block
+    # * ... Maximum consecutive 0 amount in the beginning of the pseudo-randomly generated block
     "HEAD_MAX_ZEROS": 2,
     # * ... All phone numbers starting with those codes will be rejected. Set it to `{""}` to disable this feature.
     "BANNED_OPERATOR_CODES": {
