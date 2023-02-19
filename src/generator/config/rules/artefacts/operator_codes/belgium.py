25f367324d5482b68402276174d5c68193e413aa
gustaveWPM <jgeorge@perso.be>
2023-02-19 23:40:35 +0100

    add: dummy Belgium value for i18n PoC

---
+++ src/generator/config/rules/artefacts/operator_codes/belgium.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from generator.internal_lib.list import uniq
+
+OPERATORS_CODES: dict = {
+    "MOBILE": uniq([
+        "999"
+    ]),
+
+    "DESK": uniq(
+        ["1", "2", "3", "4", "5"]
+    )
+}
