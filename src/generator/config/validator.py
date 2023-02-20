053927a3af094d0330adca63756fbea69840f285
gustaveWPM <jgeorge@perso.be>
2023-02-20 23:45:19 +0100

    review: typo

--- src/generator/config/validator.py
+++ src/generator/config/validator.py
@@ -97,10 +97,10 @@ def on_build_check_target_options(options: dict) -> Void:
         terminate(f"{_MSG_PREFIX} Target options 'DESK' and 'MOBILE' are both setted to False.")
 
     if start_with_desk and not target_option_desk:
-        terminate(f"{_MSG_PREFIX} Target options 'DESK' setted to False, but 'START_WITH_DESK_OPERATOR_CODES' setted to True.")
+        terminate(f"{_MSG_PREFIX} Target option 'DESK' setted to False, but 'START_WITH_DESK_OPERATOR_CODES' setted to True.")
 
     if start_with_mobile and not target_option_mobile:
-        terminate(f"{_MSG_PREFIX} Target options 'DESK' setted to True, but 'START_WITH_DESK_OPERATOR_CODES' setted to False.")
+        terminate(f"{_MSG_PREFIX} Target option 'DESK' setted to True, but 'START_WITH_DESK_OPERATOR_CODES' setted to False.")
 
 
 def on_build_check_targeted_country(country: str) -> Void:
