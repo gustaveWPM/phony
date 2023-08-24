18654a1271e57a161ab33595dff916d388c028b1
gustaveWPM <jgeorge@perso.be>
2023-08-24 03:07:23 +0200

    add: minimalist Python version checker on run

--- src/run.py
+++ src/run.py
@@ -1,8 +1,22 @@
 # coding: utf-8
 
 
-import generator.phone_number_generator as generator
+import sys
 
+
+__APP_NAME = "Phony"
+
+
+def check_requirements():
+    py_required_version = (3, 8, 10)
+    if sys.version_info < py_required_version:
+        py_version_str = "{}.{}.{}".format(*py_required_version)
+        sys.exit("{} requires Python >= {}".format(__APP_NAME, py_version_str))
+
+def run():
+    import generator.phone_number_generator as app
+    app.run()
 
 if __name__ == "__main__":
-    generator.run()
+    check_requirements()
+    run()
